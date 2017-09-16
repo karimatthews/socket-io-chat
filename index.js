bf502b632bcc0309942c54a65c0ea8b3b9212e73
Kari Matthews <karimatthews@gmail.com>
2017-09-16 20:41:55 +1000

    align users's messages to right

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ io.on('connection', function(socket){
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    socket.broadcast.emit('chat message', msg);
   });
 
 });
--- static/client.js
+++ static/client.js
@@ -3,10 +3,12 @@ $(function () {
   var socket = io(); // magic library stuff
   $('form').submit(function(){
     socket.emit('chat message', $('#m').val()); // key: 'chat message' value 'form input'
+    $('#messages').append($('<li class = right>').text($('#m').val())); // append the message when the user presses enter
+
     $('#m').val(''); // clears the message input form
     return false; // don't do any form realted things that would normally happen
   });
   socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+    $('#messages').append($('<li>').text(msg)); // adds the new message to the messages list
   });
 });
