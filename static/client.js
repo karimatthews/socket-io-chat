bd4405a6ed81d7d90fe419399c99cf7106eae084
Kari Matthews <karimatthews@gmail.com>
2017-09-16 21:46:30 +1000

    Update styles of messages

--- static/client.js
+++ static/client.js
@@ -3,12 +3,19 @@ $(function () {
   var socket = io(); // magic library stuff
   $('form').submit(function(){
     socket.emit('chat message', $('#m').val()); // key: 'chat message' value 'form input'
-    $('#messages').append($('<li class = right>').text($('#m').val())); // append the message when the user presses enter
-
+    $('#messages').append(
+      $('<div>').addClass("message-container"
+    ).append(
+      $('<div>').text($('#m').val()).addClass("message right")
+    )); // append the message when the user presses enter
     $('#m').val(''); // clears the message input form
     return false; // don't do any form realted things that would normally happen
   });
   socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg)); // adds the new message to the messages list
+    $('#messages').append(
+      $('<div>').addClass("message-container"
+    ).append(
+      $('<div>').text(msg).addClass("message")
+    )); // adds the new message to the messages list
   });
 });
