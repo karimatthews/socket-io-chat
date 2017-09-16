20a096249faf3dd413eea69f9d62a717de6fa5e3
Kari Matthews <karimatthews@gmail.com>
2017-09-16 16:23:09 +1000

    Broadcast a message to connected users when someone connects or disconnects

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.broadcast.emit('chat message','a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    io.emit('chat message', 'a user disconnected');
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
+
 });
 
 http.listen(3000, function(){
