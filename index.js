ccc94dcc1432786e52273d17d8329c2bbd7f796a
Kari Matthews <karimatthews@gmail.com>
2017-09-17 20:31:41 +1000

    Listen port given by heroku in prod

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000', process.env.PORT);
 });
