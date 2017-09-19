cb5c302f5b7f956ce482942d97de25b04c232708
Kari Matthews <karimatthews@gmail.com>
2017-09-19 15:07:57 +1000

    Load correct username client-side

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
 app.use(express.static('static'));
 
 app.get('/', function(req, res){
@@ -11,15 +12,27 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+
+  //Handle connection updates
   console.log('a user connected');
   socket.broadcast.emit('chat update','A user connected');
+
+  //Handle disconnection updates
   socket.on('disconnect', function(){
     console.log('user disconnected');
     io.emit('chat update', 'A user disconnected');
   });
+
+  // Broadcast messages sent by other users
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     socket.broadcast.emit('chat message', msg);
+  });
+
+  // Broadcast usernames of messages sent by other users
+  socket.on('add username', function(msg){
+    console.log('username: ' + msg);
+    socket.broadcast.emit('add username', msg);
   });
 
 });
--- static/client.js
+++ static/client.js
@@ -13,22 +13,18 @@ $(function () {
   //Set nickname of user
   $('#nameForm').submit(function(){
     username = $('#nameInput').val();   //set username varaible
-
     $('#nameForm').remove();   //remove nameForm div
 
-    $('#messages').append(
+    $('#messages').append( //adds welcome message
       $('<div>').addClass("message-container"
     ).append( //
       $('<div>').text('Hi ' + username + ', welcome to chat application.').addClass("update")
     ));
 
     $("#m").prop('disabled', false);  // enable message form input
-
     $('#messages').css('opacity', '1.0')   // update opacity of message form
     $('#messageForm').css('opacity', '1.0')   // update opacity of message form
-
     $('#m').focus(); //focus on message input
-
     return false;   // don't do any form related things that would normally happen
   });
 
@@ -37,7 +33,9 @@ $(function () {
     //If form input is empty don't print message
     if ($('#m').val() == "") {return false;}
 
-    socket.emit('chat message', $('#m').val()); // key: 'chat message' value 'form input'
+    socket.emit('add username', username); // Sends key: 'add username' value 'username'
+    socket.emit('chat message', $('#m').val()); // Sends key: 'chat message' value 'form input'
+
     $('#messages').append(
       $('<div>').addClass("message-container"
     ).append(   // append the message when the user presses enter
@@ -45,9 +43,7 @@ $(function () {
     ));
 
     updateScroll();
-
     $('#m').val(''); // clears the message input form
-
     return false; // don't do any form related things that would normally happen
   });
 
@@ -55,10 +51,20 @@ $(function () {
   socket.on('chat message', function(msg){
     $('#messages').append(
       $('<div>').addClass("message-container"
-    ).append( //
-      $('<div>').text(username).addClass("display-username")
-    ).append( // adds the new message to the messages list
+    )
+    .append( // adds the new message to the messages list
       $('<div>').text(msg).addClass("message")
+    ));
+
+    updateScroll();
+  });
+
+  //Handle usernames
+  socket.on('add username', function(msg){
+    $('#messages').append(
+      $('<div>').addClass("message-container"
+    ).append( // adds the new message to the messages list
+      $('<div>').text(msg).addClass("display-username")
     ));
 
     updateScroll();
